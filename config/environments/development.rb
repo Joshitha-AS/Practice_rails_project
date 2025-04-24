a06ec22eb328d57713c360e1b4d1ea3f3e398b79
Joshitha Adaikalam <joshitha@temps-MacBook-Pro.local>
2025-04-24 12:53:33 +0530

    updated basic ecommerce website features

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -84,6 +84,4 @@ config.action_mailer.delivery_method = :letter_opener
 config.action_mailer.perform_deliveries = true
 config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
-
-
 end
