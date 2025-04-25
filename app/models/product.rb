6c59c5f093dca3bbc0f9fe5efb98f2c78baee722
Joshitha Adaikalam <joshitha@temps-MacBook-Pro.local>
2025-04-25 15:26:05 +0530

    product model validation updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
-    validates :name, :price, presence: true
-    validates :price, numericality: true
+    validates :name,
+            presence: true,
+            length: { in: 2..100 }
+
+  validates :price,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
     
-
-
     has_many :cart_items
     has_many :carts, through: :cart_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,19 @@
 class User < ApplicationRecord
     has_secure_password
-  
+
     validates :email,
               presence: true,
               uniqueness: true,
               format: { with: URI::MailTo::EMAIL_REGEXP }
-  
+
     validates :name,
               presence: true,
               length: { in: 2..50 }
-  
+
     validates :password,
               length: { in: 6..10 },
               if: -> { new_record? || !password.nil? }
-  
+
     has_one :cart
     has_many :orders
-  end
-  
+end
