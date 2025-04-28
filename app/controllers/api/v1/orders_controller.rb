b7e3378455ea0aed791fbee2340f4a42c4ada089
Joshitha Adaikalam <joshitha@temps-MacBook-Pro.local>
2025-04-28 15:16:26 +0530

    cache added to product and orders

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,13 +1,22 @@
 module Api::V1
   class OrdersController < ApplicationController
     def index
-      @orders = @current_user.orders.includes(:order_items)
-      render json: @orders, include: :order_items
+      cache_key = "user_#{@current_user.id}_orders_json"
+
+      orders_json = Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
+        puts "====> CACHE MISS: Generating fresh orders JSON <====="
+
+        orders = @current_user.orders.includes(:order_items)
+        orders.as_json(include: :order_items).to_json
+      end
+
+      render json: orders_json
     end
 
     def create
       @order = @current_user.orders.new(order_params)
-      if @order.save
+      if @order.save!
+        Rails.cache.delete("user_#{@current_user.id}_orders_json") # Clear JSON cache
         OrderConfirmationWorker.perform_async(@order.id)
         render json: @order, status: :created
       else
@@ -18,7 +27,7 @@ module Api::V1
     
     private
     def order_params
-      params.require(:order).permit(order_items_attributes: [:product_id, :quantity, :price])
+      params.require(:order).permit(order_items_attributes: [:product_id, :price, :quantity])
     end
   end
 end
--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class ProductsController < ApplicationController
-      before_action :set_product, only: [ :show, :update, :destroy ]
+      before_action :set_product, only: [:show, :update, :destroy]
 
       def index
         products = Product.all
@@ -9,20 +9,23 @@ module Api
       end
 
       def create
-        product=Product.new(product_params)
+        product = Product.new(product_params)
         if product.save
-          render json: { message: "product created successfully" }, status: :created
+          render json: { message: "Product created successfully" }, status: :created
         else
           render json: { errors: product.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       def show
-        render json: @product
+        render json: @product.as_json(
+          only:[:name,:price]
+        )
       end
 
       def update
         if @product.update(product_params)
+          Rails.cache.delete(cache_key(@product.id))
           render json: @product
         else
           render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
@@ -30,13 +33,22 @@ module Api
       end
 
       def destroy
+        Rails.cache.delete(cache_key(@product.id))
         @product.destroy
         head :no_content
       end
 
       private
+
       def set_product
-        @product = Product.find(params[:id])
+        product_id = params[:id]
+        @product = Rails.cache.fetch(cache_key(product_id), expires_in: 5.minutes) do
+          Product.find(product_id)
+        end
+      end
+
+      def cache_key(product_id)
+        "product/#{product_id}"
       end
 
       def product_params
